c4af042aa8cba044e304ba05a681c16f8638dc25
Hengsheng-Liu <lhs7714@bu.edu>
2023-03-06 16:09:50 -0500

    try it out

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Hello = (props) => {
+    console.log(props)
+    return (
+        <div>
+            <p>Hello {props.name}, your age is {props.age}</p>
+        </div>
+    )
+}
+const Friend = (props) => {
+
+}
+const MyComponent = () => {
+    return (
+        <div>
+            <p>greetings from footer</p>
+        </div>
+    )
+}
+const App = () => {
+    const now = new Date()
+    const friends = [
+        {name: 'Donham', age: 18},
+        {name: 'Peter', age: 28}
+    ]
+    return (
+        <div>
+            <p>Hello the current time is, {now.toString()}</p>
+            <p>Hello, my name is {friends[0].name}, and my age is {friends[0].age}</p>
+            <p>Hello, my name is {friends[1].name}, and my age is {friends[1].age}</p>
+            <MyComponent/>
+        </div>
+        )
 }
 
 export default App;
