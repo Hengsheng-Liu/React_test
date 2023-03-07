0b1711229501ddfe72e0d72929b37a9edb7f6646
Hengsheng-Liu <lhs7714@bu.edu>
2023-03-06 23:33:19 -0500

    add some array in the project

--- src/App.js
+++ src/App.js
@@ -1,34 +1,51 @@
 
-const Hello = (props) => {
-    console.log(props)
+const Header = (props) => {
     return (
         <div>
-            <p>Hello {props.name}, your age is {props.age}</p>
+            <h1>{props.course}</h1>
         </div>
     )
 }
-const Friend = (props) => {
-
-}
-const MyComponent = () => {
+const Content = (props) => {
     return (
         <div>
-            <p>greetings from footer</p>
+            {props.parts.map(part =>
+            <p>{part.name} {part.exercises}</p>
+            )}
         </div>
     )
+}
+const Sum_up = (props) => {
+    let acc = 0
+        props.total.map(value => {
+            acc+=value.exercises  // numbers 1, -1, 3, 5 are printed, each to own line
+        })
+        return(<div>
+            <p>Number of exercises {acc}</p>
+        </div>)
 }
 const App = () => {
-    const now = new Date()
-    const friends = [
-        {name: 'Donham', age: 18},
-        {name: 'Peter', age: 28}
+    const course = 'Half Stack application development'
+    const parts = [
+        {
+            name: 'Fundamentals of React',
+            exercises: 10
+        },
+        {
+            name: 'Using props to pass data',
+            exercises: 7
+        },
+        {
+            name: 'State of a component',
+            exercises: 14
+        }
     ]
+
     return (
         <div>
-            <p>Hello the current time is, {now.toString()}</p>
-            <p>Hello, my name is {friends[0].name}, and my age is {friends[0].age}</p>
-            <p>Hello, my name is {friends[1].name}, and my age is {friends[1].age}</p>
-            <MyComponent/>
+            <Header course = {course}/>
+            <Content parts = {parts}/>
+            <Sum_up total = {parts}/>
         </div>
         )
 }
